59bb8f07ce248efa18cea30c74efe7140d91f13f
Emiel van den Brink <emielvdbrink@gmail.com>
2019-01-06 22:18:25 +0100

    added more logic

--- UnitySource/NFCGame/Assets/Scripts/GameController.cs
+++ UnitySource/NFCGame/Assets/Scripts/GameController.cs
@@ -19,13 +19,13 @@ public class GameController : MonoBehaviour
     public PopupDialog popupMessage;
     public FlatCharUIController[] characterUIControllers;
     public Text roundText;
-    public List<VariableChange> registeredEffects = new List<VariableChange>();
 
     private int currentPlayer;
     private int currentRound;
     private AbilityData currentData;
     private bool dialogUp;
 
+    public List<VariableChange> registeredEffects = new List<VariableChange>();
     private List<int> affectedPlayers = new List<int>();
 
     /// <summary>
@@ -37,7 +37,6 @@ public class GameController : MonoBehaviour
 
         choiceDialog.DeactivateDialogBox();
         popupMessage.gameObject.SetActive(false);
-
         dialogUp = false;
 
         currentPlayer = 0;
@@ -268,12 +267,6 @@ public class GameController : MonoBehaviour
 
     private IEnumerator ParseCoroutine(JArray effects)
     {
-        //loop through the effects
-        //get targets based on "targets" and "targetsmultiple"
-        //(after damage prompt) register all changes
-        //if offset of varchange is greater than 0, add it to dictionary
-        //if turns of varchange is greater greater than 1 add it to dictionary
-        //register changes in ui
         foreach (JToken effect in effects)
         {
             JObject effectObj = effect.ToObject<JObject>();
@@ -281,19 +274,26 @@ public class GameController : MonoBehaviour
             JArray varChanges = (JArray)effectObj.GetValue("varchng");
             affectedPlayers.Clear();
 
-            if(target == "user") //skip the targeting step
+            // get specified targets for this effect
+            if (target == "user") //skip the targeting step
             {
                 affectedPlayers.Add(currentPlayer);
             }
-
-            if (target != "user") //won't be implemented for next build
+            else //allies won't be implemented for next build
             {
                 string targetType = effectObj.GetValue("trgtType").ToString();
                 if(targetType == "multiple" || targetType == "one")
                 {
-                    List<int> selectablePlayers = new List<int>(new int[] { 0, 1, 2, 3 });
-                    selectablePlayers.Remove(currentPlayer);
-                    choiceDialog.ActivateDialogBox((targetType == "multiple"), selectablePlayers.ToArray(), "test", "this is a test");
+                    List<int> targetPlayers = new List<int>();
+                    for (int i = 0; i < AppManager.INSTANCE.characterData.Count; i++)
+                    {
+                        if (i == currentPlayer)
+                            continue;
+                        if (AppManager.INSTANCE.characterData[i].isAlive && AppManager.INSTANCE.characterData[i].currentHealth > 0)
+                            targetPlayers.Add(i);
+                    }
+
+                    choiceDialog.ActivateDialogBox((targetType == "multiple"), targetPlayers.ToArray(), "test", "this is a test");
                     dialogUp = true;
 
                     while (dialogUp)
@@ -310,14 +310,14 @@ public class GameController : MonoBehaviour
                 }
             }
 
-            /*foreach (JToken varChange in varChanges)
+            // parse variable changes to struct form
+            foreach (JToken varChange in varChanges)
             {
+                // parse to more manageable struct form
                 JObject varChangeObj = varChange.ToObject<JObject>();
-
                 VariableChange changeData = new VariableChange();
 
-                changeData.player = currentPlayer;
-                changeData.additive = varChangeObj.GetValue("type").ToString() == "additive";
+                changeData.additive = (varChangeObj.GetValue("type").ToString() == "additive");
                 changeData.change = int.Parse(varChangeObj.GetValue("chng").ToString());
                 changeData.offset = int.Parse(varChangeObj.GetValue("offst").ToString());
                 changeData.turns = int.Parse(varChangeObj.GetValue("trns").ToString());
@@ -328,26 +328,196 @@ public class GameController : MonoBehaviour
                 if (var == "victory") changeData.variable = VarType.victory;
                 if (var == "damage") changeData.variable = VarType.damage;
 
-                registeredEffects.Add(changeData);
-            }*/
+                // add to list for each player
+                foreach(int player in affectedPlayers)
+                {
+                    changeData.player = player;
+                    registeredEffects.Add(changeData);
+                }            
+            }
         }
-        yield return null;
+        ApplyVarChanges();
+    }
+
+    /// <summary>
+    /// Applies registered changes to the players and updates the registered effects
+    /// </summary>
+    private void ApplyVarChanges()
+    {
+        for (int i = registeredEffects.Count - 1; i >= 0; i--)
+        {
+            VariableChange varChange = registeredEffects[i];
+
+            if (varChange.variable == VarType.ability || varChange.variable == VarType.damage)
+                continue;
+
+            //if its a victory point modifier, apply it when its applied(depending on offset),
+            //and each players turn
+            //if its a health point modifier, apply it when its applied(depending on offset),
+            //and each players turn
+
+            if (varChange.offset > 0)
+            {
+                varChange.offset--;
+                registeredEffects[i] = varChange;
+                continue;
+            }
+
+
+            varChange.turns--;
+            if(varChange.turns <= 0)
+            {
+                registeredEffects.RemoveAt(i);
+            }
+            else
+            {
+                registeredEffects[i] = varChange;
+            }
+
+            Debug.Log("applying damage");
+            switch (varChange.variable)
+            {
+                case VarType.health:
+                    AppManager.INSTANCE.characterData[varChange.player].AddToHealth(varChange.change, false);
+                    break;
+                case VarType.victory:
+                    AppManager.INSTANCE.characterData[varChange.player].AddToVictoryPoints(varChange.change);
+                    break;
+            }
+        }
+
+        EndUpdate();
+    }
+
+    /// <summary>
+    /// This is called when its a players turn, and looks through/applies the registerd effects for any buffs/debuffs that apply
+    /// to this player
+    /// </summary>
+    private void ApplyOnTurnModifiers()
+    {
+        for (int i = registeredEffects.Count - 1; i >= 0; i--)
+        {
+            VariableChange varChange = registeredEffects[i];
+
+            //if its an ability modifier and target player is not currentplayer, apply it but to not modify it.
+            //if its a damage modifier and target player is not currentplayer, apply it but to not modify it.
+            if (varChange.variable == VarType.ability || varChange.variable == VarType.damage)
+            {
+                //apply varchange then count down and remove if turns is 0 or smaller then
+
+                if (varChange.player != currentPlayer)
+                {
+                    Debug.Log("skipping " + varChange.variable + " for player " + varChange.player + " as it's not his turn yet");
+                    continue;
+
+                }
+
+                //check if this varchange first needs to be skipped
+                if (varChange.offset > 0)
+                {
+                    varChange.offset--;
+                    registeredEffects[i] = varChange;
+                    continue;
+                }
+
+                Debug.Log("remove 1 turn for " + varChange.variable + " for player " + varChange.player);
+                varChange.turns--;
+                if (varChange.turns <= 0)
+                {
+                    registeredEffects.RemoveAt(i);
+                }
+                else
+                {
+                    registeredEffects[i] = varChange;
+                }
+            }
+        }
     }
 }
 
+/// <summary>
+/// Container class which holds all player-related variables and methods of changing them
+/// </summary>
 [System.Serializable]
 public class PlayerData
 {
     public string name;
     public string description;
+
     public int maxHealth;
     public int currentHealth;
+
     public int maxAbilityPoints;
     public int currentAbilityPoints;
+
     public int victoryPoints;
 
     public int lives;
     public bool isAlive;
+
+    public int damageAdditive;
+
+    /// <summary>
+    /// Changes the players health by the specified amount
+    /// </summary>
+    /// <param name="change">The amount with which the players health changes</param>
+    /// <param name="allowOverFlow"></param>
+    /// <returns>Int signifying if the player state, where 1 is still alive, 0 is a life lost and -1 is dead</returns>
+    public int AddToHealth(int change, bool allowOverFlow)
+    {
+        currentHealth += change;
+
+        if (currentHealth > maxHealth)
+            currentHealth = maxHealth;
+
+        else if (currentHealth <= 0)
+        {
+            if (lives - 1 < 0)
+            {
+                isAlive = false;
+                return -1;
+            }
+            else
+            {
+                lives--;
+                currentHealth = maxHealth;
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    /// <summary>
+    /// Changes the ability point additive on the player
+    /// </summary>
+    /// <param name="change"></param>
+    public void AddToAbilityPoints(int change)
+    {
+        currentAbilityPoints += change;
+        if (currentAbilityPoints < 0)
+            currentAbilityPoints = 0;
+    }
+
+    /// <summary>
+    /// Changes the players victory points by the specified amount
+    /// </summary>
+    /// <param name="change"></param>
+    /// <param name="allowOverFlow"></param>
+    public void AddToVictoryPoints(int change)
+    {
+        victoryPoints += change;
+        if (victoryPoints < 0)
+            victoryPoints = 0;
+    }
+
+    /// <summary>
+    /// Changes the damage point additive on the player
+    /// </summary>
+    /// <param name="change"></param>
+    public void AddToDamage(int change)
+    {
+        damageAdditive += change;
+    }
 }
 
 public struct AbilityData
