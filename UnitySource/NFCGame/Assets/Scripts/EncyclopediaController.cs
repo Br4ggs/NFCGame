50942e3bc99bae7ca1273a14b033042d055df9d3
Emiel van den Brink <emielvdbrink@gmail.com>
2019-01-10 10:51:42 +0100

    added animation for encyclopedia module

--- UnitySource/NFCGame/Assets/Scripts/EncyclopediaController.cs
+++ UnitySource/NFCGame/Assets/Scripts/EncyclopediaController.cs
@@ -12,10 +12,19 @@ public class EncyclopediaController : MonoBehaviour
     public Text nameTxt;
     public Text descriptionTxt;
     public Text typeTxt;
+    public GameObject messageBox;
+
+    public Vector3 offScreenPos;
+    private Vector3 onScreenPos;
+
+    private bool inAnimation;
 
     void OnEnable()
     {
         AppManager.INSTANCE.OnValidJsonRecieved += OnDataRecievedHandler;
+        onScreenPos = messageBox.transform.localPosition;
+
+        messageBox.transform.localPosition = offScreenPos;
     }
 
     void OnDisable()
@@ -25,8 +34,40 @@ public class EncyclopediaController : MonoBehaviour
 
     public void OnDataRecievedHandler(object sender, JObject e)
     {
+        if (inAnimation)
+            return;
+
+        StartCoroutine(AnimationCoRoutine(e));
+        inAnimation = true;
+    }
+
+    public IEnumerator AnimationCoRoutine(JObject e)
+    {
+        while (Vector3.Distance(messageBox.transform.localPosition, offScreenPos) > 0.05f)
+        {
+            messageBox.transform.localPosition = Vector3.Lerp(messageBox.transform.localPosition, offScreenPos, 0.2f);
+            yield return null;
+        }
+        messageBox.transform.localPosition = offScreenPos;
+
         nameTxt.text = e.GetValue("name").ToString();
-        descriptionTxt.text = e.GetValue("description").ToString();
-        typeTxt.text = e.GetValue("typeOf").ToString();
+
+        JToken desc = e.GetValue("description");
+        if (desc == null)
+            desc = e.GetValue("desc");
+        descriptionTxt.text = desc.ToString();
+
+        JToken typ = e.GetValue("typeOf");
+        if (typ == null)
+            typ = e.GetValue("type");
+        typeTxt.text = typ.ToString();
+
+        while (Vector3.Distance(messageBox.transform.localPosition, onScreenPos) > 0.05f)
+        {
+            messageBox.transform.localPosition = Vector3.Lerp(messageBox.transform.localPosition, onScreenPos, 0.2f);
+            yield return null;
+        }
+        messageBox.transform.localPosition = onScreenPos;
+        inAnimation = false;
     }
 }
